8d217dda3f449c74acc1f29a10278480f6db14fb
Eric Horne (Intuit) <eric_horne@intuit.com>
2013-06-18 12:16:15 -0700

    version update to 1.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'eric_horne@intuit.com'
 license          'All rights reserved'
 description      'Installs/Configures erichorne-intuit-myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
